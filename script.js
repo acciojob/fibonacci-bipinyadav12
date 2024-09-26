a0f35230ed8aad29c44657a7be51e354d0f70e63
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-09-26 16:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function fibonacci(num) {
-let a = 0, b = 1, c ,i ;
+
 	if (num===1){
-		return a;
+		return 0;
 	}
 	else if (num===2){
-		return b;
+		return 1;
 	}
+	let a=0 ,b=1;
 	for( i=3; i<=num ;i++){
-		c=a+b;
-		b=c;
+	let	temp=a+b;
+		b=temp;
 		a=b;
 	}
 			return b;

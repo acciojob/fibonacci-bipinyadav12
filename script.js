1831f797a596208115db71d0f331e86ceeac84a8
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-09-26 16:17:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function fibonacci(num) {
 let a=0,b=1,c=1;
 	if (num<1){
-		return 0;
+		return a;
 	}
 	for(let i=1; i<=n;i++){
 		c=a+b;
 		b=c;
-		console.log(c,);
+		a=b;
 	}
+			return b;
 }
 
 module.exports = fibonacci;
